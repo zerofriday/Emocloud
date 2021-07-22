8427b8f2fdef1d195e9edec02a3439cf614f3011
friday <workFriday@protonmail.com>
2021-07-22 05:56:23 -0700

    feat: refactor api client

--- lib/app/data/repository/SiaRepository.dart
+++ lib/app/data/repository/SiaRepository.dart
@@ -1,10 +1,10 @@
-import 'package:cloud/app/core/network/api_client.dart';
+import 'package:cloud/app/core/network/api_emo_client.dart';
 import 'package:dio/dio.dart';
 
 class SiaRepository {
   late Dio dio;
 
   SiaRepository() {
-    dio = ApiClient().dio;
+    dio = ApiEmoClient().dio;
   }
 }
